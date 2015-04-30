e1b51752b7effcc090d569b40a6dc5116b7aeb25
jrichter42 <mail@jensrichter.info>
2015-04-30 11:12:14 +0200

    Workarounds durch Camera->GetForward() ersetzt

--- src/SpaceBrickArena/EditorScene.cpp
+++ src/SpaceBrickArena/EditorScene.cpp
@@ -69,12 +69,7 @@ namespace Game
 
         this->m_pCamera->Update(a_pGraphics, a_pWindow, a_pInput, a_pTimer);
 
-        //Calculate the camera direction (muss in der engine ordentlich umgesetzt werden und dann hier ersetzt werden!)
-        PuRe_Vector3F cameraLook = PuRe_Vector3F(0, 0, 1);
-        PuRe_Vector3F cameraRotation = this->m_pCamera->GetRotation();
-        cameraLook = cameraLook * PuRe_QuaternionF(cameraRotation.X*0.0174532925f, cameraRotation.Y*0.0174532925f, cameraRotation.Z*0.0174532925f).GetMatrix();
-
-        this->m_pBrickWorker->Update(a_pGraphics, a_pWindow, a_pInput, a_pTimer, a_pSoundPlayer, cameraLook);
+        this->m_pBrickWorker->Update(a_pGraphics, a_pWindow, a_pInput, a_pTimer, a_pSoundPlayer, this->m_pCamera->GetForward());
 
         for (int32 i = 0; i < 4; i++)
         {
